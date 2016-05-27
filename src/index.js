80f8b1d23e7347e5e2645117b01c4b29b729a658
John <jmogielnicki@gmail.com>
2016-05-27 14:25:09 -0700

    input box and js file structure created

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyC2GctOTTYTeHLlpN23JrWTI5AtsSqrX_Q'
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
+
 // Create a new component.  THis component should produce
-// some html
-
-
+// some HTML
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
 
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
